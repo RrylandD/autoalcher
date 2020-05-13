6ecbf7f9d0808debf3257f0f2648243118094554
Ryland Donovan <RrylandD@gmail.com>
2020-05-13 03:55:22 -0400

    oh no

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rylanddonovan/Documents/github/Code/python/alch"
+set -gx VIRTUAL_ENV "/Users/rylanddonovan/Documents/github/autoalcher"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(alch) "
-            printf "%s%s" "(alch) " (set_color normal)
+        if test -n "(autoalcher) "
+            printf "%s%s" "(autoalcher) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
